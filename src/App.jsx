e61ed4f15fd1ca97cf89f295014d738a0970d347
Juan Estay <juan.estay.rodriguez@icloud.com>
2025-09-08 13:54:28 -0300

    update punto de venta render sin blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-import Blog from "./pages/Blog";
 import PuntoDeVenta from "./Pages/PuntosDeVentas"
 import "./App.css";
 
@@ -9,8 +8,6 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        {/* Ruta blog */}
-        <Route path="/blog" element={<Blog />} />
          {/* Ruta puntos de venta */}
         <Route path="/puntos-venta" element={<PuntoDeVenta />} />
       </Routes>
